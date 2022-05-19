2bbdf99c67ccab5f9dff003a80a1ee127e3cbe16
greenmelonkid <lag8675@gmail.com>
2022-05-19 17:08:55 -0500

    adding files

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -7,9 +7,9 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //  Add a marker to the map for Los Angeles, California.
 L.circleMarker([34.0522, -118.2437], {
-    radius: 100,
-    fillColor: "#ffffa1",
-    color: "black"
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
